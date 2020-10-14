8f8851d67a79fccaec670e3f0621a7f3aced9bb5
cgn <cgn1874@163.com>
2020-10-14 22:10:00 +0800

    成功运行train、val、test

--- teacher-student.py
+++ teacher-student.py
@@ -8,23 +8,10 @@ import tensorflow.compat.v1 as tf
 import numpy as np
 import os, sys
 import time
+import _pickle as cPickle
+from math import ceil
 
 tf.disable_v2_behavior()
-
-# 全局参数
-global model, task, lr, epoch, dropout, noisy,  Nratio, Nsigma, KD, lamda, tau, batchsize
-model = 'savemodel'  # model_path to save the student model\n In testing, give trained student model.", type=str)
-task = 'test'  # 'task for this file, train/test/val'
-lr = 1e-3
-epoch = 100
-dropout = 0.5
-noisy = False  # weather add noisy to logits (noisy-teacher model')
-Nratio = 0.5  # noisy ratio
-Nsigma = 0.9  # noisy sigma
-KD = False  # (knowledge distilling, hinton 2014')
-lamda = 0.3  # 'KD method. lamda between original loss and soft-target loss.', type=float)
-tau = 3.0  # KD method. tau stands for temperature
-batchsize = 256
 
 
 class bcolors:
@@ -46,10 +33,12 @@ def conv(x, W, b, strides=1, padding='SAME'):
     x = tf.nn.bias_add(x, b)
     return x
 
+
 def maxpool2d(x, k, s, padding='SAME'):
     # MaxPool2D wrapper
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, s, s, 1],
                           padding=padding)
+
 
 def avgpool2d(x, k, s, padding='SAME'):
     # AvgPool2D wrapper
@@ -66,33 +55,33 @@ global x,y,keep_prob
 def nin(): # modify from model - network in network
 
     # pre-trained weight
-    npyfile = np.load('teacher.npy')
+    npyfile = np.load('teacher.npy',allow_pickle=True, encoding='bytes')
     npyfile = npyfile.item()
 
     weights = {
-        'conv1': tf.Variable(npyfile['conv1']['weights'], trainable=False, name = 'conv1_w'),
-        'cccp1': tf.Variable(npyfile['cccp1']['weights'], trainable=False, name = 'cccp1_w'),
-        'cccp2': tf.Variable(npyfile['cccp2']['weights'], trainable=False, name = 'cccp2_w'),
-        'conv2': tf.Variable(npyfile['conv2']['weights'], trainable=False, name = 'conv2_w'),
-        'cccp3': tf.Variable(npyfile['cccp3']['weights'], trainable=False, name = 'cccp3_w'),
-        'cccp4': tf.Variable(npyfile['cccp4']['weights'], trainable=False, name = 'cccp4_w'),
-        'conv3': tf.Variable(npyfile['conv3']['weights'], trainable=False, name = 'conv3_w'),
-        'cccp5': tf.Variable(npyfile['cccp5']['weights'], trainable=False, name = 'cccp5_w'),
-        'ip1': tf.Variable(npyfile['ip1']['weights'], trainable=False, name = 'ip1_w'),
-        'ip2': tf.Variable(npyfile['ip2']['weights'], trainable=False, name = 'ip2_w')
+        'conv1': tf.Variable(npyfile['conv1'][b'weights'], trainable=False, name = 'conv1_w'),
+        'cccp1': tf.Variable(npyfile['cccp1'][b'weights'], trainable=False, name = 'cccp1_w'),
+        'cccp2': tf.Variable(npyfile['cccp2'][b'weights'], trainable=False, name = 'cccp2_w'),
+        'conv2': tf.Variable(npyfile['conv2'][b'weights'], trainable=False, name = 'conv2_w'),
+        'cccp3': tf.Variable(npyfile['cccp3'][b'weights'], trainable=False, name = 'cccp3_w'),
+        'cccp4': tf.Variable(npyfile['cccp4'][b'weights'], trainable=False, name = 'cccp4_w'),
+        'conv3': tf.Variable(npyfile['conv3'][b'weights'], trainable=False, name = 'conv3_w'),
+        'cccp5': tf.Variable(npyfile['cccp5'][b'weights'], trainable=False, name = 'cccp5_w'),
+        'ip1': tf.Variable(npyfile['ip1'][b'weights'], trainable=False, name = 'ip1_w'),
+        'ip2': tf.Variable(npyfile['ip2'][b'weights'], trainable=False, name = 'ip2_w')
     }
 
     biases = {
-        'conv1': tf.Variable(npyfile['conv1']['biases'], trainable=False, name = 'conv1_b'),
-        'cccp1': tf.Variable(npyfile['cccp1']['biases'], trainable=False, name = 'cccp1_b'),
-        'cccp2': tf.Variable(npyfile['cccp2']['biases'], trainable=False, name = 'cccp2_b'),
-        'conv2': tf.Variable(npyfile['conv2']['biases'], trainable=False, name = 'conv2_b'),
-        'cccp3': tf.Variable(npyfile['cccp3']['biases'], trainable=False, name = 'cccp3_b'),
-        'cccp4': tf.Variable(npyfile['cccp4']['biases'], trainable=False, name = 'cccp4_b'),
-        'conv3': tf.Variable(npyfile['conv3']['biases'], trainable=False, name = 'conv3_b'),
-        'cccp5': tf.Variable(npyfile['cccp5']['biases'], trainable=False, name = 'cccp5_b'),
-        'ip1': tf.Variable(npyfile['ip1']['biases'], trainable=False, name = 'ip1_b'),
-        'ip2': tf.Variable(npyfile['ip2']['biases'], trainable=False, name = 'ip2_b')
+        'conv1': tf.Variable(npyfile['conv1'][b'biases'], trainable=False, name = 'conv1_b'),
+        'cccp1': tf.Variable(npyfile['cccp1'][b'biases'], trainable=False, name = 'cccp1_b'),
+        'cccp2': tf.Variable(npyfile['cccp2'][b'biases'], trainable=False, name = 'cccp2_b'),
+        'conv2': tf.Variable(npyfile['conv2'][b'biases'], trainable=False, name = 'conv2_b'),
+        'cccp3': tf.Variable(npyfile['cccp3'][b'biases'], trainable=False, name = 'cccp3_b'),
+        'cccp4': tf.Variable(npyfile['cccp4'][b'biases'], trainable=False, name = 'cccp4_b'),
+        'conv3': tf.Variable(npyfile['conv3'][b'biases'], trainable=False, name = 'conv3_b'),
+        'cccp5': tf.Variable(npyfile['cccp5'][b'biases'], trainable=False, name = 'cccp5_b'),
+        'ip1': tf.Variable(npyfile['ip1'][b'biases'], trainable=False, name = 'ip1_b'),
+        'ip2': tf.Variable(npyfile['ip2'][b'biases'], trainable=False, name = 'ip2_b')
     }
 
     conv1 = conv(x, weights['conv1'], biases['conv1'])
@@ -132,10 +121,10 @@ def lenet(use_pretrained=False): # modify from lenet model
     if use_pretrained == False:
         # Random initialize
         weights = {
-            'conv1': tf.get_variable('LN_conv1_w', [5,5,3,64],initializer=tf.contrib.layers.xavier_initializer_conv2d()),
-            'conv2': tf.get_variable('LN_conv2_w', [5,5,64,128],initializer=tf.contrib.layers.xavier_initializer_conv2d()),
-            'ip1': tf.get_variable('LN_ip1_w', [5*5*128, 1024] , initializer=tf.contrib.layers.xavier_initializer()),
-            'ip2': tf.get_variable('LN_ip2_w', [1024,10], initializer=tf.contrib.layers.xavier_initializer())
+            'conv1': tf.get_variable('LN_conv1_w', [5,5,3,64],initializer=tf.uniform_unit_scaling_initializer()),
+            'conv2': tf.get_variable('LN_conv2_w', [5,5,64,128],initializer=tf.uniform_unit_scaling_initializer()),
+            'ip1': tf.get_variable('LN_ip1_w', [5*5*128, 1024] , initializer=tf.uniform_unit_scaling_initializer()),
+            'ip2': tf.get_variable('LN_ip2_w', [1024,10], initializer=tf.uniform_unit_scaling_initializer())
         }
 
         biases = {
@@ -175,9 +164,8 @@ def lenet(use_pretrained=False): # modify from lenet model
 
 # Reading cifar dataset
 def unpickle(file):
-    import cPickle
     fo = open(file, 'rb')
-    dict = cPickle.load(fo)
+    dict = cPickle.load(fo, encoding='bytes')
     fo.close()
     return dict
 
@@ -189,7 +177,7 @@ def cal_mean():
 
 def read_cifar10(flag):
     # Directory of cifar-10
-    path = '/home/james/Desktop/workspace/model_compression/cifar-10-batches-py'
+    path = './cifar10/cifar-10-batches-py'
     if flag == 'train':
         batches = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
         data = np.uint8(np.zeros((50000,32,32,3)))
@@ -204,23 +192,21 @@ def read_cifar10(flag):
         b = batches[i]
         temp = unpickle(os.path.join(path,b))
         for j in range(N):
-            data[N*i+j][:,:,2] = np.reshape(temp['data'][j][2048:],(32,32))
-            data[N*i+j][:,:,1] = np.reshape(temp['data'][j][1024:2048],(32,32))
-            data[N*i+j][:,:,0] = np.reshape(temp['data'][j][:1024],(32,32))
-            label[N*i+j] = temp['labels'][j]
+            data[N*i+j][:,:,2] = np.reshape(temp[b'data'][j][2048:],(32,32))
+            data[N*i+j][:,:,1] = np.reshape(temp[b'data'][j][1024:2048],(32,32))
+            data[N*i+j][:,:,0] = np.reshape(temp[b'data'][j][:1024],(32,32))
+            label[N*i+j] = temp[b'labels'][j]
     return data, label
 
 
-def train():
-    learning_rate = lr
-    model_path=model
+def train(model_path, learning_rate, epoch, noisy=False):
     total_epoch = epoch
     teacher=nin()
     student=lenet()
     if noisy == True:
         drop_scale = 1 / Nratio
-        noisy_mask = tf.nn.dropout( tf.constant(np.float32(np.ones((batch_size,1)))/drop_scale) ,keep_prob=args.Nratio) #(batchsize,1)
-        gaussian = tf.random_normal(shape=[batch_size,1], mean=0.0, stddev=args.Nsigma)
+        noisy_mask = tf.nn.dropout( tf.constant(np.float32(np.ones((batch_size,1)))/drop_scale) ,keep_prob=Nratio) #(batchsize,1)
+        gaussian = tf.random_normal(shape=[batch_size,1], mean=0.0, stddev=Nsigma)
         noisy = tf.mul(noisy_mask, gaussian)
         #noisy_add = tf.add(tf.constant(np.float32(np.ones((batch_size,1)))), noisy)
         teacher = tf.mul(teacher, tf.tile(noisy,tf.constant([1,10])))   #(batchsize,10)
@@ -231,11 +217,11 @@ def train():
         print(bcolors.G+"prepare for training, knowledge distilling mode"+bcolors.END)
         one_hot = tf.one_hot(y, n_classes,1.0,0.0)
         #one_hot = tf.cast(one_hot_int, tf.float32)
-        teacher_tau = tf.scalar_mul(1.0/args.tau, teacher)
-        student_tau = tf.scalar_mul(1.0/args.tau, student)
+        teacher_tau = tf.scalar_mul(1.0/tau, teacher)
+        student_tau = tf.scalar_mul(1.0/tau, student)
         objective1 = tf.nn.sigmoid_cross_entropy_with_logits(student_tau, one_hot)
         objective2 = tf.scalar_mul(0.5, tf.square(student_tau-teacher_tau))
-        tf_loss = (args.lamda*tf.reduce_sum(objective1) + (1-args.lamda)*tf.reduce_sum(objective2))/batch_size
+        tf_loss = (lamda*tf.reduce_sum(objective1) + (1 - lamda) * tf.reduce_sum(objective2)) / batch_size
     else:
         print(bcolors.G+"prepare for training, NIPS2014 mode"+bcolors.END)
         tf_loss = tf.nn.l2_loss(teacher - student)/batch_size
@@ -253,10 +239,10 @@ def train():
     index=np.array(range(len(data)))    # index randomly ordered
     mean = cal_mean()
     begin = time.time()
-    iterations = len(data)/batch_size
+    iterations = len(data) // batch_size
     decay_step = int(total_epoch*0.8)
     cnt=0
-    dropout_rate=args.dropout
+    dropout_rate = dropout
     print(bcolors.G+"number of iterations (per epoch) ="+str(len(data)/batch_size)+bcolors.END)
     for i in range(total_epoch):
         np.random.shuffle(index)
@@ -298,16 +284,17 @@ def test_net():
     sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
     init=tf.initialize_all_variables()
     sess.run(init)
-    with tf.device('/cpu:0'):
+    with tf.device('/gpu:0'):
         saver = tf.train.Saver()
-        saver.restore(sess, args.model)
+        model_file = tf.train.latest_checkpoint(model)
+        saver.restore(sess, model_file)
 
     mean = cal_mean()
     data, label=read_cifar10('test')
     total=0
     correct=0
     begin = time.time()
-    for j in range(len(data)/batch_size):
+    for j in range(len(data)//batch_size):
         batch_x = data[j*batch_size:(j+1)*batch_size] - mean
         prob = sess.run([pred],
                 feed_dict={x : batch_x, y : np.ones((batch_size)), keep_prob : 1.0})
@@ -332,7 +319,7 @@ def valid_nin():
     total=0
     correct=0
     begin = time.time()
-    for j in range(len(data)/batch_size):
+    for j in range(len(data)//batch_size):
         batch_x = data[j*batch_size:(j+1)*batch_size] - mean
         prob = sess.run([pool3],
                 feed_dict={x : batch_x, y : np.ones((batch_size)), keep_prob : 1.0})
@@ -344,6 +331,21 @@ def valid_nin():
 
 
 if __name__ == '__main__':
+    # 参数设置
+    model = 'savemodel'  # model_path to save the student model\n In testing, give trained student model.", type=str)
+    task = 'test'  # 'task for this file, train/test/val'
+    lr = 1e-3
+    epoch = 100
+    dropout = 0.5
+    noisy = False  # weather add noisy to logits (noisy-teacher model')
+    Nratio = 0.5  # noisy ratio
+    Nsigma = 0.9  # noisy sigma
+    KD = False  # (knowledge distilling, hinton 2014')
+    lamda = 0.3  # 'KD method. lamda between original loss and soft-target loss.', type=float)
+    tau = 3.0  # KD method. tau stands for temperature
+    batch_size = 256
+
+    # 检查参数
     if noisy == True and KD == True:
         print(bcolors.BOLD+bcolors.R+
               "Invalid args!\n"+
@@ -359,7 +361,7 @@ if __name__ == '__main__':
     keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
     with tf.device('/gpu:0'):
         if task=='train':
-            train()
+            train(model, learning_rate=lr, epoch=epoch)
         elif task=='test':
             test_net()
         elif task=='val':
